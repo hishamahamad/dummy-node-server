1e7adb756fa54565f20eeca7dc718f5a110ec893
Hisham Ahamad <hisham.ahamad@blackbuck.com>
2020-09-21 03:53:16 +0530

    added header

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ app.use(CookieParser());
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
