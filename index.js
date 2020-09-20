da5f2cd804ed79ec8e9901df44f60a3d641a0ccb
Hisham Ahamad <hisham.ahamad@blackbuck.com>
2020-09-21 03:22:04 +0530

    changes

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const app = express();
 
 app.use(CookieParser());
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 app.get('/', (request, response) => {
+  console.log(request.cookies);
   response.json({
     leads: [
       {id: 1, lead_name: 'Ramu'},
