ad11f6e3ada067abea6270d2cf8336dff789041d
Hisham Ahamad <hisham.ahamad@blackbuck.com>
2020-09-21 18:00:29 +0530

    changes

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(cors());
 // });
 
 app.get('/', (request, response) => {
+  console.log('cookies', request.cookies);
   const auth = request.headers.authorization;
   if(auth) {
     response.json({
